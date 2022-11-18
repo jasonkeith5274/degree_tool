3be4e88c2fe5fcd17d50eee0269bd91725e43b86
Zubair Shaik <zubyshaik08@gmail.com>
2022-11-17 21:51:09 -0600

    basic button and form

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function Home() {
+    return (
+      <div>
+        <p>
+            Hey! Praniggalous here
+          </p>
+      </div>
+    );
+  }
+  
+  export default Home;
