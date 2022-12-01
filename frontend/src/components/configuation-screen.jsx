4e3e8945b1bb8a4ae7a812dd03f971660bbcfaa6
Jason Keith <jasonkeith5274@gmail.com>
2022-12-01 17:32:20 -0600

    making sure everything is up to date on github

--- frontend/src/components/configuation-screen.jsx
+++ frontend/src/components/configuation-screen.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import * as React from "react";
+import {useState, useEffect} from 'react'
+import axios from "axios" 
   
-const Configuration = () => {
+function Configuration() {
+  
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    async function fetchClasses() {
+      const URL = 'http://localhost:8000/api/courses/';
+      try {
+        const res = await axios.get(URL);
+        console.log(res.data);
+        setClasses(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchClasses();
+  }, []);
+
+  if (!classes.length) return <h3>Loading...</h3>;
+  
+
+  const columns = [{  
+    Header: 'Class Number',  
+    accessor: ''  
+    },{  
+    Header: 'Age',  
+    accessor: 'age'  
+    }]  
+
+
   return (
     <div>
-      <h1>Configuration</h1>
+      
     </div>
   );
-};
+}
   
 export default Configuration;
